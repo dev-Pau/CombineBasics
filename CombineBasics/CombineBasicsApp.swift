17fa704dce4153a5df758a1ecdf1783c8f7e33ba
Pau <paucryptoc@gmail.com>
2024-08-07 15:53:05 +0200

    Initial Commit

---
+++ CombineBasics/CombineBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineBasicsApp.swift
+//  CombineBasics
+//
+//  Created by Pau Fernández Solà on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
